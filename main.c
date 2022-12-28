0a9b06784c4f5929d2ca6fa0a93a0a9bf2fe7e3d
gioele-ferrari <gioeleferrari203@gmail.com>
2022-12-28 15:03:04 +0100

    new algo

--- main.c
+++ main.c
@@ -9,6 +9,8 @@ void stampa_maggiore(int A[], int n, int x);
 int count_maggiore(int A[], int n, int x);
 int find(int A[], int n, int x);
 int find_index(int A[], int n, int x, int k);
+void add_condizione(int A[], int n, int x, int y);
+int inserisci_coda(int A[], int n, int x);
 
 int main(int argc, char const *argv[])
 {
@@ -20,8 +22,10 @@ int main(int argc, char const *argv[])
      * l'elemento all'estremo del vettore è -1. Nel caso ci fossero
      * elementi oltre al delimitatore saranno ignorati.
     */
-    int A[VET_TAPPO] = {1, 2, 3, 3, 5, 6, 7, -1};
-    load_vettore(A, VET_TAPPO);
+    int A[VET_TAPPO] = {1, 2, 3, 3, 5, 6, -1};
+    int new_index = inserisci_coda(A, VET_TAPPO, 7);
+    printf("Nuovo indice: %d\n", new_index);
+    stampa_vettore(A, VET_TAPPO);
     return 0;
 }
 
@@ -113,7 +117,6 @@ int count_maggiore(int A[], int n, int x) {
         }
         i++;
     }
-
     return count;
 }
 
@@ -166,6 +169,50 @@ int find_index(int A[], int n, int x, int k) {
             i++;
         }
     }
-    
     return i;
 }
+
+/**
+ * @param A vettore in cui effettuare la modifica.
+ * @param n la dimensione del vettore.
+ * @param x numero da aggiungere.
+ * @param y numero da superare.
+ * L'algoritmo ha un complessità di O(m) dove m è il numero di elementi
+ * mentre l'invariante è tutti gli elementi del vettore da posizione 0 a posizione
+ * A[i] (escluso) maggiori di y sommati a x.
+ * @return nessun return.
+*/
+void add_condizione(int A[], int n, int x, int y) {
+    int i = 0;
+    while (A[i] != -1) {
+        if(A[i] > y) {
+            A[i] += x;
+        }
+        i++;
+    }
+}
+
+/**
+ * @param A vettore in cui effettuare la ricerca.
+ * @param n la dimensione del vettore.
+ * @param x numero da inserire.
+ * L'algoritmo ha un complessità di O(m) dove m è il numero di elementi.
+ * @attention inserire invariante.
+ * @return l'indice del nuovo elemento, -1 se non si può inserire.
+*/
+int inserisci_coda(int A[], int n, int x) {
+    int i = 0;
+    while (A[i] != -1) {
+        i++;
+    }
+    
+    if(i < n - 1) {
+        A[i] = x;
+        A[i + 1] = -1;
+    }
+    else {
+        i = -1;
+    }
+    return i; 
+}
+
